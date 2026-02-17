b2cb2ad1765b39959de6b61640a179740f4307d9
codebyMaurica <m.bellaphant@outlook.com>
2026-02-16 20:06:57 -0600

    Learned Js

---
+++ Week - 5/index.js
@@ -0,0 +1,13 @@
+// integers (numbers)
+console.log(1500+1500); //add
+console.log(1500-1500); //subtract
+console.log(1500*1500); //multiply
+console.log(1500/1500); //divide
+// string (human words) & variables
+let name ="Maurica";
+
+console.log(name)
+console.log(name)
+console.log(name)
+console.log(name)
+console.log(name)
