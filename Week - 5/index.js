6d10fd4d5d1fff555e606ca186b5f7468ba6eeac
codebyMaurica <m.bellaphant@outlook.com>
2026-02-17 20:06:23 -0600

    Learned Functions

--- Week - 5/index.js
+++ Week - 5/index.js
@@ -1,13 +1,68 @@
 // integers (numbers)
-console.log(1500+1500); //add
-console.log(1500-1500); //subtract
-console.log(1500*1500); //multiply
-console.log(1500/1500); //divide
+console.log(1500 + 1500); //add
+console.log(1500 - 1500); //subtract
+console.log(1500 * 1500); //multiply
+console.log(1500 / 1500); //divide
 // string (human words) & variables
-let name ="Maurica";
+let name = "Maurica";
+let age = 36;
+let food = "Salad";
+console.log(name)
+console.log(name)
+console.log(name)
+console.log(name)
+console.log(name)
 
-console.log(name)
-console.log(name)
-console.log(name)
-console.log(name)
-console.log(name)
+console.log(age)
+console.log("My name is " + name + " and I am " + age + " year old and i love to eat " + food + ".");
+
+//functions//
+
+
+
+function sayHello() {
+    console.log("hello");
+    console.log("hello");
+    console.log("hello");
+}
+
+sayHello();
+
+
+function addNumbers(a, b) {
+    console.log(a + b);
+}
+
+addNumbers(9, 3);
+addNumbers(3, 5);
+addNumbers(5, 1);
+
+function deleteNumbers(num1, num2) {
+    console.log(num1 - num2);
+}
+
+deleteNumbers(12, 6);
+deleteNumbers(15, 5);
+deleteNumbers(50, 25);
+
+function timesNumbers(num1, num2) {
+    console.log(num1 * num2);
+}
+
+timesNumbers(5, 3);
+timesNumbers(6, 5);
+timesNumbers(5, 9);
+
+function divideNumbers(num1, num2) {
+    console.log(num1 / num2);
+}
+
+divideNumbers(25, 5);
+divideNumbers(100, 10);
+divideNumbers(350, 50);
+
+let favColor = "teal";
+favColor = "blue"
+console.log(favColor);
+favColor = "green";
+console.log(favColor);
