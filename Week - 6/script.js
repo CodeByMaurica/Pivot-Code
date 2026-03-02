e0d32b5feb4f8a6f7c0d2e0aab5b62f710defca5
codebyMaurica <m.bellaphant@outlook.com>
2026-03-02 00:55:25 -0600

    Learning More JS

--- Week - 6/script.js
+++ Week - 6/script.js
@@ -7,3 +7,32 @@ food = "pizza";
 function sayHello() {
   alert("Hello " + name);
 }
+
+
+function loginHandler() {
+  const nameInput = document.getElementById("name-input");
+  const emailInput = document.getElementById("email-input");
+  console.log("Your name is: " + nameInput.value);
+  console.log("Your email is:" + emailInput.value);
+}
+
+
+function changeText() {
+  const changeText = document.getElementById("change-text");
+
+  if (changeText.textContent === "Hello") {
+    changeText.textContent = "Bye!!";
+  } else {
+    changeText.textContent = "Hello";
+  }
+}
+
+function changeColor() {
+  const changeText = document.getElementById("change-text");
+
+  if (changeText.style.color === "red") {
+    changeText.style.color = "black";
+  } else {
+    changeText.style.color = "red";
+  }
+}
